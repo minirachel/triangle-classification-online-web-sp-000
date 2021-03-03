01b76e9e60734cb3772ce46e75b657a307b1ec52
minirachel <rachel.a.levinson@gmail.com>
2021-03-03 13:44:36 -0500

    Done.

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,48 @@
+require 'pry'
+
 class Triangle
-  # write code here
+
+  attr_accessor :side1, :side2, :side3
+
+
+
+  def initialize (side1, side2, side3)
+    @triangle = []
+    @triangle << side1
+    @triangle << side2
+    @triangle << side3
+  end
+
+  def valid?
+    sum_one_two = @triangle[0] + @triangle[1]
+    sum_one_three = @triangle[0] + @triangle[2]
+    sum_two_three = @triangle[1] + @triangle[2]
+
+    @triangle.none?{|s| s <= 0} && sum_one_two > @triangle[2] && sum_one_three > @triangle[1] && sum_two_three > @triangle[0]
+  end
+
+  # @side1 == @side2 && @side2 == @side3
+  # @side1 != @side2 && @side2 != @side3 && @side3 != @side1
+
+  def kind
+    if valid?
+      if @triangle.uniq.length == 1
+        :equilateral
+      elsif @triangle.uniq.length == 2
+        :isosceles
+      else
+        :scalene
+      end
+    else
+      raise TriangleError
+    end
+  end
+
+  class TriangleError < StandardError
+
+  end
 end
+
+
+
+
